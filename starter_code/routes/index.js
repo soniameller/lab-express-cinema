a8b25553517c81fe70499e7e13ffd2a110cfd1a3
Sonia Meller <mellersonia@gmail.com>
2019-06-27 20:19:50 +0100

    done, no style

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const Movie = require("../models/Movie");
+const router = express.Router();
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find().then(moviesFromDb=>{
+    res.render("movies",{
+      movie: moviesFromDb
+    });
+  });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  let movieId = req.params.movieId
+  Movie.findById(movieId)
+    .then(movieInfo => {
+      res.render("movie-detail", { 
+        movie: movieInfo });
+    })
 });
 
 module.exports = router;
